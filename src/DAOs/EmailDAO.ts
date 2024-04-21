d0357c7baacae88d79a3b3fcc9626237b403b738
Ajraa <salajkapavel@gmail.com>
2024-04-22 00:34:57 +0200

    Added EmailDAO

---
+++ src/DAOs/EmailDAO.ts
@@ -0,0 +1,81 @@
+import { Prisma, Email } from "@prisma/client";
+import { DefaultArgs } from "@prisma/client/runtime/library";
+
+export class EmailDAO {
+  private _emails: Prisma.EmailDelegate<DefaultArgs>;
+
+  constructor(emails: Prisma.EmailDelegate<DefaultArgs>) {
+    this._emails = emails;
+  }
+
+  public async sendEmail(
+    from: number,
+    to: number,
+    title: string,
+    content: string,
+  ): Promise<ServerResponse<Email>> {
+    return await this._emails.create({
+      data: {
+        fromId: from,
+        toId: to,
+        title: title,
+        content: content,
+      },
+    })
+    .then(email => {
+        return new ServerResponse(200, "Email sent", email);
+    })
+    .catch(err => {
+        if (err instanceof Error) console.log(err.message);
+        return new ServerResponse<Email>(500, "Server side error", null);
+    })
+  }
+
+  public async loadAcviteEmails(userId: number): Promise<ServerResponse<Email[]>> {
+    return await this._emails.findMany({
+        where: {
+            toId: userId,
+            deleted: false
+        }
+    })
+    .then(emails => {
+        return new ServerResponse<Email[]>(200, "Email sent", emails);
+    })
+    .catch(err => {
+        if (err instanceof Error) console.log(err.message);
+        return new ServerResponse<Email[]>(500, "Server side error", null);
+    })
+  }
+
+  public async loadDeletedEmails(userId: number): Promise<ServerResponse<Email[]>> {
+    return await this._emails.findMany({
+        where: {
+            toId: userId,
+            deleted: true
+        }
+    })
+    .then(emails => {
+        return new ServerResponse<Email[]>(200, "Email sent", emails);
+    })
+    .catch(err => {
+        if (err instanceof Error) console.log(err.message);
+        return new ServerResponse<Email[]>(500, "Server side error", null);
+    })
+  }
+
+  public async deleteMail(userId: number, mailId: number): Promise<ServerResponse<null>> {
+    return await this._emails.delete({
+        where: {
+            toId: userId,
+            id: mailId
+        }
+    })
+    .then(() => {
+        return new ServerResponse<null>(200, "Email deleted", null);
+    })
+    .catch(err => {
+        if (err instanceof Error) console.log(err.message);
+        return new ServerResponse<null>(500, "Server side error", null);
+    })
+  }
+}
