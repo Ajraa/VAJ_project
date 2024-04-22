837dae95955941b99f14efa8d7fbc89974ec009e
Ajraa <salajkapavel@gmail.com>
2024-04-22 22:51:46 +0200

    Added read bool to email

---
+++ prisma/migrations/20240422205124_added_read_email/migration.sql
@@ -0,0 +1,18 @@
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Email" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "fromId" INTEGER NOT NULL,
+    "toId" INTEGER NOT NULL,
+    "deleted" BOOLEAN NOT NULL DEFAULT false,
+    "read" BOOLEAN NOT NULL DEFAULT false,
+    CONSTRAINT "Email_fromId_fkey" FOREIGN KEY ("fromId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Email_toId_fkey" FOREIGN KEY ("toId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_Email" ("content", "deleted", "fromId", "id", "title", "toId") SELECT "content", "deleted", "fromId", "id", "title", "toId" FROM "Email";
+DROP TABLE "Email";
+ALTER TABLE "new_Email" RENAME TO "Email";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
